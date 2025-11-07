4d2da1b94985cdc72e6d0d91faeec2ae34f5e27b
Nick Jones <nick.jones@arachnid.co.nz>
2025-11-08 12:46:19 +1300

    Updating the clojure version.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure     "1.10.1"]
+  :dependencies [[org.clojure/clojure     "1.12.1"]
                  [clj-commons/fs           "1.5.0"]
                  [org.clojure/tools.cli    "0.4.2"]
                  [com.mpatric/mp3agic      "0.9.1"]
@@ -13,14 +13,15 @@
   :target-path "target/%s" 
 
   :profiles {:dev {:source-paths ["dev"]
-                    :dependencies [[org.clojure/tools.namespace "0.2.10"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.10"]
                                   [org.clojure/java.classpath   "0.3.0"] 
-                                  [cljfmt                       "0.6.4"]
                                   [org.openjfx/javafx-fxml      "12.0.1"]
                                   [org.openjfx/javafx-controls  "12.0.1"]
                                   [org.openjfx/javafx-swing     "12.0.1"]
                                   [org.openjfx/javafx-base      "12.0.1"]
-                                  [org.openjfx/javafx-web       "12.0.1"]]
-                    :plugins      [[lein-ancient "0.6.15"]
-                                   [lein-cljfmt   "0.6.4"]]}
+                                  [org.openjfx/javafx-web       "12.0.1"]
+                                  [com.bhauman/rebel-readline   "0.1.4"]]
+                   :plugins      [[lein-ancient "0.6.15"]
+                                  [lein-cljfmt   "0.6.4"]]
+                   :repl-options {:init-ns user}}
              :uberjar {:aot :all}})
